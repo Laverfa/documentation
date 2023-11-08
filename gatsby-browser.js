e459dca8619e6823440f3534cc028e0da318cdf9
Laverfa <143363935+Laverfa@users.noreply.github.com>
2023-11-08 01:42:24 -0600

    Delete gatsby-browser.js

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-export {default as wrapPageElement} from './src/page'
-export {default as wrapRootElement} from './src/root'
-
-export const shouldUpdateScroll = ({routerProps}) => {
-  const {scrollUpdate = true} = routerProps.location.state ?? {}
-  return scrollUpdate
-}
